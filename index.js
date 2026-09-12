e8e42fff994264cd01e09576d47ae03a2cfb6ad8
mobiflix <mobiflixhub@gmail.com>
2026-09-12 18:09:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-// Nuvio Plugin Entry — walang async/await sa top level
+/**
+ * Nuvio Multi-Embed Provider
+ * Hermes-compatible — walang async/await, arrow functions, o const/let.
+ */
+
 var MOVIE_ENDPOINTS = [
   'https://vidlink.pro/movie/',
   'https://vidsrc.dev/embed/movie/',
@@ -24,62 +28,100 @@ var SERIES_ENDPOINTS = [
   'https://moviesapi.club/tv/'
 ];
 
-function extractSource(url) {
+function extractDomain(url) {
   try {
-    var host = url.replace('https://', '').replace('http://', '').split('/')[0];
+    var host = url.replace('https://', '').replace('http://', '');
+    host = host.split('/')[0];
     host = host.replace('www.', '');
-    return host.split('.')[0].charAt(0).toUpperCase() + host.split('.')[0].slice(1);
+    var name = host.split('.')[0];
+    return name.charAt(0).toUpperCase() + name.slice(1);
   } catch (e) {
     return 'Unknown';
   }
 }
 
-function getStreams(params) {
-  var tmdbId = params.tmdbId;
-  var mediaType = params.mediaType;
-  var season = params.season || 1;
-  var episode = params.episode || 1;
-
+function buildStreams(tmdbId, mediaType, season, episode) {
   var streams = [];
+  var i;
+  var endpoint;
+
+  if (typeof season === 'undefined') season = 1;
+  if (typeof episode === 'undefined') episode = 1;
 
   if (mediaType === 'movie') {
-    for (var i = 0; i < MOVIE_ENDPOINTS.length; i++) {
-      var endpoint = MOVIE_ENDPOINTS[i];
+    for (i = 0; i < MOVIE_ENDPOINTS.length; i++) {
+      endpoint = MOVIE_ENDPOINTS[i];
       streams.push({
-        name: extractSource(endpoint),
-        title: extractSource(endpoint) + ' — HD',
+        source: extractDomain(endpoint),
         url: endpoint + tmdbId,
         quality: 'HD',
-        type: 'iframe',
-        provider: 'MultiEmbed'
+        type: 'iframe'
       });
     }
   } else if (mediaType === 'tv') {
-    for (var j = 0; j < SERIES_ENDPOINTS.length; j++) {
-      var ep = SERIES_ENDPOINTS[j];
-      var url;
-      if (ep.indexOf('2embed.cc/embedtvfull') !== -1) {
-        url = ep + tmdbId;
-      } else if (ep.indexOf('vidsrc.me') !== -1) {
-        url = ep + tmdbId + '/' + season + '/' + episode;
+    for (i = 0; i < SERIES_ENDPOINTS.length; i++) {
+      endpoint = SERIES_ENDPOINTS[i];
+
+      if (endpoint.indexOf('2embed.cc/embedtvfull') !== -1) {
+        streams.push({
+          source: '2Embed (Full)',
+          url: endpoint + tmdbId,
+          quality: 'HD',
+          type: 'iframe'
+        });
+      } else if (endpoint.indexOf('vidsrc.me') !== -1) {
+        streams.push({
+          source: 'VidSrc.me',
+          url: endpoint + tmdbId + '/' + season + '/' + episode,
+          quality: 'HD',
+          type: 'iframe'
+        });
       } else {
-        url = ep + tmdbId + '/' + season + '/' + episode;
+        streams.push({
+          source: extractDomain(endpoint),
+          url: endpoint + tmdbId + '/' + season + '/' + episode,
+          quality: 'HD',
+          type: 'iframe'
+        });
       }
-      streams.push({
-        name: extractSource(ep),
-        title: extractSource(ep) + ' — HD',
-        url: url,
-        quality: 'HD',
-        type: 'iframe',
-        provider: 'MultiEmbed'
-      });
     }
   }
 
   return streams;
 }
 
-// Export — ito ang hinahanap ng Nuvio
+function getStreams(params) {
+  var tmdbId = params.tmdbId;
+  var mediaType = params.mediaType;
+  var season = params.season;
+  var episode = params.episode;
+  var streams = buildStreams(tmdbId, mediaType, season, episode);
+
+  if (!streams.length) {
+    return [];
+  }
+
+  var result = [];
+  for (var i = 0; i < streams.length; i++) {
+    var s = streams[i];
+    result.push({
+      name: s.source,
+      title: s.source + ' - ' + s.quality,
+      url: s.url,
+      quality: s.quality,
+      type: s.type,
+      headers: {
+        'Referer': s.url,
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+      },
+      provider: 'MultiEmbed',
+      index: i
+    });
+  }
+
+  return result;
+}
+
 module.exports = {
   getStreams: getStreams
 };
